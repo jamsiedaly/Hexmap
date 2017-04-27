1c914dc72b6167f8e8536a38be8eb822a7bc6992
James Daly <14152789@studentmail.ul.ie>
2017-04-27 20:45:56 +0100

    offset cells

--- Assets/HexGrid.cs
+++ Assets/HexGrid.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 
 public class HexGrid : MonoBehaviour
 {
@@ -10,8 +11,14 @@ public class HexGrid : MonoBehaviour
 
     HexCell[] cells;
 
+    public Text cellLabelPrefab;
+
+    Canvas gridCanvas;
+
     void Awake()
     {
+        gridCanvas = GetComponentInChildren<Canvas>();
+
         cells = new HexCell[height * width];
 
         for (int z = 0, i = 0; z < height; z++)
@@ -26,12 +33,18 @@ public class HexGrid : MonoBehaviour
     void CreateCell(int x, int z, int i)
     {
         Vector3 position;
-        position.x = x * 10f;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
         position.y = 0f;
-        position.z = z * 10f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
 
         HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
+
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition =
+            new Vector2(position.x, position.z);
+        label.text = x.ToString() + "\n" + z.ToString();
     }
 }
