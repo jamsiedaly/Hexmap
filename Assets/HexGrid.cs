dbd89f86a56ec5a8b2e920f698693b6d0e87671b
James Daly <14152789@studentmail.ul.ie>
2017-04-30 22:32:57 +0100

    added GUI components

--- Assets/HexGrid.cs
+++ Assets/HexGrid.cs
@@ -5,8 +5,7 @@ public class HexGrid : MonoBehaviour
 {
 
     public int width = 30;
-    public int height = 30;
-
+    public int height = 30; 
     public HexCell cellPrefab;
 
     HexCell[] cells;
@@ -47,11 +46,12 @@ public class HexGrid : MonoBehaviour
         HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
 
         Text label = Instantiate<Text>(cellLabelPrefab);
         label.rectTransform.SetParent(gridCanvas.transform, false);
         label.rectTransform.anchoredPosition =
             new Vector2(position.x, position.z);
-        label.text = x.ToString() + "\n" + z.ToString();
+        label.text = cell.coordinates.ToStringOnSeparateLines();
     }
 }
